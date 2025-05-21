f02747726f8f201a0d880d9ca9b8ac4b674ffe5a
Mehmet Maytahan <74978898+MehmetMaytahan@users.noreply.github.com>
2025-05-21 13:24:10 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "Web Tasarımcısıyım",
+    "Grafik Tasarımcısıyım",
+    "Web Geliştiricisiyim",
+    "Youtuberım",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
