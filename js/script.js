d414d0de981522574c14b964c52bda5f1fd5cdd9
Mehmet MAYTAHAN <maytahan71@gmail.com>
2025-05-30 18:24:04 +0300

    eksikler giderildi.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+/* ===== typing animation ===== */
 var typed = new Typed(".typing", {
-  strings: [
-    "Web Tasarımcısıyım",
-    "Grafik Tasarımcısıyım",
-    "Web Geliştiricisiyim",
-    "Youtuberım",
-  ],
+  strings: ["Jr. Web Geliştiricisiyim", "", "Jr. Web Tasarımcısıyım"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
 });
+
+/* ===== Aside ===== */
+
+const nav = document.querySelector(".nav"),
+  navLİst = nav.querySelectorAll("li"),
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length,
+  totalNavList = navLİst.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navLİst[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      navLİst[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+  });
+}
+
+const showSection = (element) => {
+  const target = element.getAttribute("href").split("#")[1];
+
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+
+  document.querySelector("#" + target).classList.add("active");
+};
