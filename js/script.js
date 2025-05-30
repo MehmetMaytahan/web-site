0bac32735cb6bf338e9730aad0991e8e1ef7d8aa
Mehmet MAYTAHAN <maytahan71@gmail.com>
2025-05-30 20:32:37 +0300

    responsive tasarım geliştirildi.

--- js/script.js
+++ js/script.js
@@ -17,11 +17,19 @@ const nav = document.querySelector(".nav"),
 for (let i = 0; i < totalNavList; i++) {
   const a = navLİst[i].querySelector("a");
   a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
+
     for (let j = 0; j < totalNavList; j++) {
+      if (navLİst[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
       navLİst[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
     showSection(this);
+    if (window.innerWidth < 1200) asideSectionTogglerBtn();
   });
 }
 
@@ -34,3 +42,18 @@ const showSection = (element) => {
 
   document.querySelector("#" + target).classList.add("active");
 };
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
